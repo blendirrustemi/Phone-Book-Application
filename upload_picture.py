daf5ddc9447990f9bcbd71827bc6d08be51b845f
Blendi Rrustemi <blendirrustemi9@gmail.com>
2022-03-26 13:49:42 +0100

    Upload Profile Picture

---
+++ upload_picture.py
@@ -0,0 +1,42 @@
+import mysql.connector
+from tkinter import *
+from tkinter import filedialog
+# from PIL import ImageTk, Image
+import search_contact
+
+
+class pic_upload:
+
+    def __init__(self, UID):
+        # self.NID = NID
+        self.UID = UID
+
+    def profile_picture(self):
+
+        root = Tk()
+        root.withdraw()
+
+        file_path = filedialog.askopenfilename()
+
+        print(file_path)
+
+        # root = Tk()
+        canvas = Canvas(root, width=300, height=300)
+        canvas.pack()
+
+        conn = mysql.connector.connect(host='localhost', port='3306', user='root', password='', database='contacts')
+        cursor = conn.cursor()
+
+        all_query = "select UID, profilepic from numbers where UID=" + str(self.UID) + ";"
+        cursor.execute(all_query)
+        records = cursor.fetchall()
+
+
+        showquery ="update numbers set profilepic='" + file_path + "'where UID =" + str(self.UID) + ";"
+        # values = (file_path)
+        cursor.execute(showquery)
+        conn.commit()
+
+        call_search_contacts = search_contact.Contacts(self.UID)
+        call_search_contacts.contact_search()
+        root.destroy()
